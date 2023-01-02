6f1307770bdddedf3c579d8699d9b780f581e3e6
ltfide <lutfidendians@gmail.com>
2023-01-02 09:55:46 +0700

    Array Diff

--- Codewars/Java/src/test/java/codewars/enamkyu/ArrayDiff.java
+++ Codewars/Java/src/test/java/codewars/enamkyu/ArrayDiff.java
@@ -3,31 +3,47 @@ package codewars.enamkyu;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 public class ArrayDiff {
 
+   /**
+    * arrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2})
+    * arrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2}) => new int[] {1, 3})
+    * arrayDiff(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 })
+    */
    public static int[] arrayDiff(int[] a, int[] b) {
-      List<int[]> listOne = Arrays.asList(a);
-      List<int[]> listTwo = Arrays.asList(b);
+      List<Integer> result = new ArrayList<>();
 
-      List<int[]> differences = new ArrayList<>(listOne);
-
-      for (int[] x : differences) {
-         for (int y : x) {
-            System.out.println(y);
+      for (int i = 0; i < a.length; i++) {
+         boolean flag = false;
+         for (int j = 0; j < b.length; j++) {
+            if (a[i] == b[j]) {
+               flag = true;
+               break;
+            }
          }
+         if (!flag)
+            result.add(a[i]);
       }
 
-      return null;
+      int[] c = new int[result.size()];
+      for (int i = 0; i < result.size(); i++) {
+         c[i] = result.get(i);
+      }
+
+      return c;
    }
 
    @Test
    void tests() {
       System.out.println(arrayDiff(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 }));
+      System.out.println(arrayDiff(new int[] { 1, 2, 2 }, new int[] { 2 }));
+      System.out.println(arrayDiff(new int[] { 1, 2 }, new int[] { 1 }));
+      System.out.println(arrayDiff(new int[] { 1, 2, 4, 5, 6, 10 }, new int[] { 5, 6, 10 }));
+      System.out.println(arrayDiff(new int[] { 1, 2, 2 }, new int[] { 1 }));
    }
 
    @Test
@@ -37,6 +53,18 @@ public class ArrayDiff {
       assertArrayEquals(new int[] { 1 }, arrayDiff(new int[] { 1, 2, 2 }, new int[] { 2 }));
       assertArrayEquals(new int[] { 1, 2, 2 }, arrayDiff(new int[] { 1, 2, 2 }, new int[] {}));
       assertArrayEquals(new int[] {}, arrayDiff(new int[] {}, new int[] { 1, 2 }));
+      assertArrayEquals(new int[] { 1, 2, 4 }, arrayDiff(new int[] { 1, 2, 4, 5, 6, 10 }, new int[] { 5, 6, 10 }));
+   }
+
+   @Test
+   void testListRemove() {
+      List<Integer> list = new ArrayList<>();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      list.remove(list.indexOf(3));
+
+      System.out.println(list.size());
    }
 
 }
