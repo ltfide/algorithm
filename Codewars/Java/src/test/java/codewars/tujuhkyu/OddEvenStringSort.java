0de7c113ae99e3b86dd7746a26fe42d2ba1d33a3
ltfide <lutfidendians@gmail.com>
2023-05-01 11:04:19 +0700

    odd-even string sort

--- Codewars/Java/src/test/java/codewars/tujuhkyu/OddEvenStringSort.java
+++ Codewars/Java/src/test/java/codewars/tujuhkyu/OddEvenStringSort.java
@@ -1,2 +1,42 @@
-package codewars.tujuhkyu;public class OddEvenStringSort {
+package codewars.tujuhkyu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author Lutfi Dendiansyah
+ * @created 01/05/2023 10:52
+ */
+public class OddEvenStringSort {
+
+    public static String sortMyString(String s) {
+        String oddString = "";
+        String evenString = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 1) {
+                oddString += s.charAt(i);
+            } else {
+                evenString += s.charAt(i);
+            }
+        }
+        return evenString + " " + oddString;
+    }
+
+    @Test
+    void test() {
+        String s = "Codewars";
+        System.out.println(sortMyString(s));
+    }
+
+    @Test
+    public void testSomething() {
+        assertEquals("Wleclgltihuebredrf ofsheesenasnegrof", sortMyString("Wolfeschlegelsteinhausenbergerdorff"));
+        assertEquals("MTINLHENLHENGUAIYAGNL EHOYTROYTROYLTMNLRIY", sortMyString("METHIONYLTHREONYLTHREONYGLUTAMINYLARGINYL"));
+        assertEquals("PEMNUTAIRSOISLCVLAOOISS NUOOLRMCOCPCIIOOCNCNOI", sortMyString("PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS"));
+        assertEquals("PEDPEDHPPRTYODS SUOSUOYOAAHRIIM", sortMyString("PSEUDOPSEUDOHYPOPARATHYROIDISM"));
+        assertEquals("FOCNUIIIIIIIAIN LCIACNHLPLFCTO", sortMyString("FLOCCINAUCINIHILIPILIFICATION"));
+        assertEquals("SBEMTGYHC UDRAOLPI", sortMyString("SUBDERMATOGLYPHIC"));
+    }
 }
