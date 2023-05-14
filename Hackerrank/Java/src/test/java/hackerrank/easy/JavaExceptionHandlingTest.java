a2147f4a37e1ef58fec20784e1f318c26ff39ad8
ltfide <lutfidendians@gmail.com>
2023-05-14 12:52:50 +0700

    java exception handling

--- Hackerrank/Java/src/test/java/hackerrank/easy/JavaExceptionHandlingTest.java
+++ Hackerrank/Java/src/test/java/hackerrank/easy/JavaExceptionHandlingTest.java
@@ -6,6 +6,25 @@ import java.util.InputMismatchException;
 
 public class JavaExceptionHandlingTest {
 
+    public static Long power(int a, int b) throws Exception {
+        if (a < 0 || b < 0) {
+            throw new Exception("n or p should not be negative.");
+        }
+
+        if (a == 0 && b == 0) {
+            throw new Exception("n and p should not be zero.");
+        }
+        Long result = (long) Math.pow(a, b);
+        return result;
+    }
+
+    @Test
+    void testPower() throws Exception {
+        System.out.println(power(3,  5));
+        System.out.println(power(-2,  -1));
+        System.out.println(power(0,  0));
+    }
+
     @Test
     void handleException() {
         try {
